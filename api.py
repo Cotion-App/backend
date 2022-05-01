24582eb86407e42c17cd974f1e90d51ca2446232
abhiram <abhiramg2021@gmail.com>
2022-04-30 23:09:59 -0400

    updated error handling in backend

--- api.py
+++ api.py
@@ -9,82 +9,74 @@ app = Flask(__name__)
 CORS(app)
 load_dotenv()
 headers = {'Authorization': os.getenv(
-        'NOTION'), 'Notion-Version': '2021-08-16'}
+    'NOTION'), 'Notion-Version': '2021-08-16'}
 
 
-@app.route('/run/<domain>/<course_id>/<course>/<db_id>')
-def run(domain, course_id, course, db_id):
+@app.route('/run/<domain>/<course_id>/<course_name>/<db_id>')
+def run(domain, course_id, course_name, db_id):
     """api endpoint that acts as a wrapper for all sub functions, and basic error handling"""
-    new_ = get_assignments(domain, course_id)['assignments']
-    if (type(new_)) == str():
-        return new_
-    curr_ = read_notion(db_id)
-    if (type(curr_)) == str():
-        return curr_
-    update_ = update_notion(db_id, new_, curr_, course)
-    if (type(update_)) == str():
-        return update_
-    return 'success'
-
-def get_assignments(domain, course_id):
     try:
-        """query the canvas api and return the current assignments as a dictionary"""
-        user_token = os.getenv('CANVAS')
-        canvas = Canvas("https://" + domain, user_token)
-        assignments = canvas.get_course(int(course_id)).get_assignments()
-        out = []
-
-        for ass in assignments:
-            out.append({'name': ass.__getattribute__('name'),
-                    'due': str(ass.__getattribute__('due_at'))[:10]})
-        return {'assignments': out}
+        new_state = get_assignments(domain, course_id)['assignments']
+        curr_state = read_notion(db_id)
+        update_notion(db_id, new_state, curr_state, course_name)
+        return 'success'
     except Exception as e:
         return str(e)
+
+
+def get_assignments(domain, course_id):
+    """query the canvas api and return the current assignments as a dictionary"""
+
+    user_token = os.getenv('CANVAS')
+    canvas = Canvas("https://" + domain, user_token)
+    assignments = canvas.get_course(int(course_id)).get_assignments()
+    out = []
+
+    for ass in assignments:
+        out.append({'name': ass.__getattribute__('name'),
+                    'due': str(ass.__getattribute__('due_at'))[:10]})
+    return {'assignments': out}
+
 
 def read_notion(db_id):
     """this function reads notion and returns the current state as a dictionary with only the relevant details"""
-    
-    try:
-        has_more = True
-        start_cursor = None
 
-        all_tasks = []
+    has_more = True
+    start_cursor = None
 
-        while has_more:
-            if start_cursor:
-                res = requests.post('https://api.notion.com/v1/databases/' + db_id +
-                                    '/query', headers=headers, json={'start_cursor': start_cursor})
-            else:
-                res = requests.post(
-                    'https://api.notion.com/v1/databases/' + db_id + '/query', headers=headers)
-            res_json = res.json()
-            all_tasks.extend(res_json['results'])
-            has_more = res_json['has_more']
-            start_cursor = res_json['next_cursor']
-        temp = {}
-        for task in all_tasks:
-            temp[task['properties']['Assignment Name']['title'][0]['text']['content']] = {
-                'id': task['id'], 'due': task['properties']['Due']['date']['start']}
-        return temp
-    except Exception as e:
-        return str(e)
+    all_tasks = []
 
-def update_notion(db_id, new_, curr_, course):
+    while has_more:
+        if start_cursor:
+            res = requests.post('https://api.notion.com/v1/databases/' + db_id +
+                                '/query', headers=headers, json={'start_cursor': start_cursor})
+        else:
+            res = requests.post(
+                'https://api.notion.com/v1/databases/' + db_id + '/query', headers=headers)
+        res_json = res.json()
+        all_tasks.extend(res_json['results'])
+        has_more = res_json['has_more']
+        start_cursor = res_json['next_cursor']
+    temp = {}
+    for task in all_tasks:
+        temp[task['properties']['Assignment Name']['title'][0]['text']['content']] = {
+            'id': task['id'], 'due': task['properties']['Due']['date']['start']}
+    return temp
+
+
+def update_notion(db_id, new_state, curr_state, course_name):
     """intelligently updates notion so that we do not have duplicate data"""
-    try:
-        for ass in new_:
-            if ass['name'] in curr_.keys():
-                payload = {'properties': {'Due':{'start':ass['due']}}}
-                requests.patch(
-                    'https://api.notion.com/v1/pages/'+curr_['name']['id'], headers=headers, json=payload)
-            else:
-                if (curr_[ass['name']]['date'] != ass['date']):
-                    payload={'parent' : {'database_id':db_id}, 
-                            'properties': {'Assignment Name' :{'title' : [{'text':{'content' : ass['name']}}]}, 
-                                                                'Due' : {'date':{'start':ass['due']}}, 
-                                                                'Course' : {'select' : {'name' : course}}}}
-                    requests.post("https://api.notion.com/v1/pages", headers=headers,json=payload)
-        return True
-    except Exception as e:
-        return str(e)
-    
+
+    for ass in new_state:
+        if ass['name'] in curr_state.keys():
+            payload = {'properties': {'Due': {'start': ass['due']}}}
+            requests.patch(
+                'https://api.notion.com/v1/pages/'+curr_state['name']['id'], headers=headers, json=payload)
+        else:
+            if (curr_state[ass['name']]['date'] != ass['date']):
+                payload = {'parent': {'database_id': db_id},
+                           'properties': {'Assignment Name': {'title': [{'text': {'content': ass['name']}}]},
+                                          'Due': {'date': {'start': ass['due']}},
+                                          'Course': {'select': {'name': course_name}}}}
+                requests.post("https://api.notion.com/v1/pages",
+                              headers=headers, json=payload)
