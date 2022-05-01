1d75301053e83444ff10eab9a94083f638ad5596
abhiram <abhiramg2021@gmail.com>
2022-05-01 14:53:17 -0400

    more error checking

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from canvasapi import Canvas
 import os
 from dotenv import load_dotenv
-
+import json
 app = Flask(__name__)
 CORS(app)
 load_dotenv()
@@ -19,23 +19,38 @@ def run(domain, canvas_token, course_id, course_name, db_id):
         new_state = get_assignments(domain, canvas_token, course_id)['assignments']
         curr_state = read_notion(db_id)
         update_notion(db_id, new_state, curr_state, course_name)
-        return 'success'
+        return 'You are now up to date!'
     except Exception as e:
-        return str(e)
-
-
+        return str(e).capitalize()
+        
 def get_assignments(domain,canvas_token, course_id):
-    """query the canvas api and return the current assignments as a dictionary"""
+    try:
+        """query the canvas api and return the current assignments as a dictionary"""
 
-    canvas = Canvas("https://" + domain, canvas_token)
-    assignments = canvas.get_course(int(course_id)).get_assignments()
-    out = []
+        canvas = Canvas("https://" + domain, canvas_token)
+        assignments = canvas.get_course(int(course_id)).get_assignments()
+        out = []
 
-    for ass in assignments:
-        out.append({'name': ass.__getattribute__('name'),
-                    'due': str(ass.__getattribute__('due_at'))[:10]})
-    return {'assignments': out}
+        for ass in assignments:
+            out.append({'name': ass.__getattribute__('name'),
+                        'due': str(ass.__getattribute__('due_at'))[:10]})
+        return {'assignments': out}
+    except Exception as e:
+        ex = str(type(e))
+        
+        # logging to console
+        print(type(e), e)
 
+        if ('Unauthorized' in ex):
+            message = "You do not have access to this class in Canvas"
+        elif ('InvalidAccessToken' in ex):
+            message = "You passed in an invalid access token"
+        else:
+            # Unhandled exception
+            print('Unhandled Canvas exception: ' + str(e))
+            message = "Unhandled Exception"
+
+        raise Exception(message)
 
 def read_notion(db_id):
     """this function reads notion and returns the current state as a dictionary with only the relevant details"""
@@ -52,30 +67,62 @@ def read_notion(db_id):
         else:
             res = requests.post(
                 'https://api.notion.com/v1/databases/' + db_id + '/query', headers=headers)
+
+        if (res.status_code != 200):
+            notion_errors(json.loads(res._content)['code'])
+
         res_json = res.json()
         all_tasks.extend(res_json['results'])
+       
         has_more = res_json['has_more']
         start_cursor = res_json['next_cursor']
     temp = {}
     for task in all_tasks:
+        date=''
+        if (task['properties']['Due']['date'] == None):
+            date = 'None'
+        else :
+            date = task['properties']['Due']['date']['start']
         temp[task['properties']['Assignment Name']['title'][0]['text']['content']] = {
-            'id': task['id'], 'due': task['properties']['Due']['date']['start']}
+            'id': task['id'], 
+            'due': task['properties']['Due']['date'] == date
+            }
     return temp
 
+def notion_errors(code):
+    """returns readable strings for any Notion errors."""
+
+    errors = {"unauthorized" : "The api token in backend is not working. Contact Abhi for a fix",
+                 "restricted_resource": "You have not shared the table with Abhi.",
+                 "object_not_found" : "You have passed in an invalid database url",
+                 "database_connection_unavailable" : "Notion API is unqueryable right now"}
+
+    try:
+        message = errors[code]
+        raise Exception(message)
+    except:
+        print(code)
+        raise Exception('Unhandled Notion Exception')
+
+    pass
 
 def update_notion(db_id, new_state, curr_state, course_name):
     """intelligently updates notion so that we do not have duplicate data"""
-
-    for ass in new_state:
-        if ass['name'] in curr_state.keys():
-            payload = {'properties': {'Due': {'start': ass['due']}}}
-            requests.patch(
-                'https://api.notion.com/v1/pages/'+curr_state['name']['id'], headers=headers, json=payload)
-        else:
-            if (curr_state[ass['name']]['date'] != ass['date']):
-                payload = {'parent': {'database_id': db_id},
-                           'properties': {'Assignment Name': {'title': [{'text': {'content': ass['name']}}]},
-                                          'Due': {'date': {'start': ass['due']}},
-                                          'Course': {'select': {'name': course_name}}}}
-                requests.post("https://api.notion.com/v1/pages",
-                              headers=headers, json=payload)
+    # equality checking, equality checking to prevent unnecessary calls, missing column issue, 
+    # better error handling (for any unknown errors its hard to pinpoint cause because of current rendering)
+    for assignment in new_state:
+        payload = {'parent': {'database_id': db_id},
+                   'properties': {'Assignment Name': {'title': [{'text': {'content': assignment['name']}}]},
+                                                      'Due': {'date': {'start': assignment['due']}},
+                                                      'Course': {'select': {'name': course_name}}}}
+        if (assignment['due'] == 'None'):
+            del payload['properties']['Due']
+        res=''
+        if assignment['name'] in curr_state.keys():
+            res = requests.patch(
+                    'https://api.notion.com/v1/pages/'+curr_state[assignment['name']]['id'], headers=headers, json=payload)
+        else:  
+            res = requests.post("https://api.notion.com/v1/pages",headers=headers, json=payload)
+        if (res.status_code != 200):
+            notion_errors(json.loads(res._content)['code'])
+    
